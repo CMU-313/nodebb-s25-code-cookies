4feae2fa3a534bcb6495d57afef31fe3f8e6f506
Ryan Chernoff <rchernof@andrew.cmu.edu>
2025-03-30 17:07:02 -0400

    Fixed error in translate function and added a proper url

--- flow/translate/index.js
+++ flow/translate/index.js
@@ -4,9 +4,9 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
     // Edit the translator URL below
-    const TRANSLATOR_API = "http://translator:5000/"
+    const TRANSLATOR_API = "http://127.0.0.1:5000";
     const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
     const data = await response.json();
-    console.log(data)
-    return [data["is_english"], data["translated_content"]]
+    console.log(data);
+    return [data["is_english"], data["translated_content"]];
 }
