3f9e3ef0351f58a781d4ef5b57a400cb1fa7001d
Ryan Chernoff <rchernof@andrew.cmu.edu>
2025-03-31 17:20:32 -0400

    Fixed lint errors in src folder as well

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,10 +1,11 @@
-var request = require('request');
+'use strict';
+
 const translatorApi = module.exports;
 translatorApi.translate = async function (postData) {
   // Edit the translator URL below
-  const TRANSLATOR_API = "http://127.0.0.1:5000";
-  const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+  const TRANSLATOR_API = 'http://127.0.0.1:5000';
+  const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
   const data = await response.json();
   console.log(data);
-  return [data["is_english"], data["translated_content"]];
+  return [data.is_english, data.translated_content];
 };
