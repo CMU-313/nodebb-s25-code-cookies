696e5fcd5822eb65deba022b2d89abe31640c850
Ryan Chernoff <rchernof@andrew.cmu.edu>
2025-04-01 11:23:33 -0400

    Fixed lint error with try catch block

--- flow/translate/index.js
+++ flow/translate/index.js
@@ -5,14 +5,14 @@ const translatorApi = module.exports;
 translatorApi.translate = async function (postData) {
 	// Edit the translator URL below
 	const TRANSLATOR_API = 'http:17313-team15.s3d.cmu.edu:5000';
-	try{
+	try {
 		const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 		const data = await response.json();
 		if (!data || data.is_english === undefined || data.translated_content === undefined) {
 			return [true, postData.content];
 		}
 		return [data.is_english, data.translated_content];
-	} catch (error){
+	} catch (error) {
 		return [true, postData.content];
 	}
 };
