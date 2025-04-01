df867e4860fbaee3cf727a46c8efe0741e6a7a47
Ryan Chernoff <rchernof@andrew.cmu.edu>
2025-03-31 21:33:21 -0400

    Fixed schema issues with test suite. It was failing due to the new attributes

--- src/translate/index.js
+++ src/translate/index.js
@@ -6,6 +6,5 @@ translatorApi.translate = async function (postData) {
   const TRANSLATOR_API = 'http:17313-team15.s3d.cmu.edu:5000';
   const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
   const data = await response.json();
-  console.log(data);
   return [data.is_english, data.translated_content];
 };
