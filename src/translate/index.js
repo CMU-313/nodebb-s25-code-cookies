efd9fda3a7ce33f587e0e19d20d0e8c6be2833a7
Jonas <jonass@andrew.cmu.edu>
2025-04-07 14:28:00 -0400

    changed link to point to local translator server

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,7 @@
 const translatorApi = module.exports;
 translatorApi.translate = async function (postData) {
   // Edit the translator URL below
-  const TRANSLATOR_API = 'http:17313-team15.s3d.cmu.edu:5000';
+  const TRANSLATOR_API = 'http://127.0.0.1:5000';
   try {
     const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
     const data = await response.json();
