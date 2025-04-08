65034659c8a64f2556928893ed9ec7ec04dc66e7
Jonas <jonass@andrew.cmu.edu>
2025-04-08 12:48:39 -0400

    changed url to local host

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,7 @@
 const translatorApi = module.exports;
 translatorApi.translate = async function (postData) {
   // Edit the translator URL below
-  const TRANSLATOR_API = 'http://127.0.0.1:5000';
+  const TRANSLATOR_API = 'localhost:5000';
   try {
     const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
     const data = await response.json();
